6a055a7c8030df8862174c906896201051daa751
Crowdbotics <team@crowdbotics.com>
2024-05-07 14:09:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-pond-47835",
+  url: "https://falling-pond-47835.botics.co",
+  api: "https://falling-pond-47835.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
